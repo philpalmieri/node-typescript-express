c120afa81babc59f44c59052cef1914a0dcd0722
Phil Palmieri <ppalmieri@partnercomm.net>
2017-08-01 13:15:21 -0400

    initial build, setup, and tests

---
+++ tests/routing.test.ts
@@ -0,0 +1,24 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import app from '../src/App';
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('baseRouter', () => {
+    it('should be json', () => {
+        return chai.request(app).get('/')
+        .then(res => {
+            expect(res.type).to.eql('application/json');
+        })
+    });
+
+    it('should have a message prop', () => {
+        return chai.request(app).get('/')
+        .then(res => {
+            expect(res.body.message).to.eql('Hello World!');
+        });
+    });
+});
